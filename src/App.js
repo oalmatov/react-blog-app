910139e01cd62914a6d120b7a6b0ea45516d52a9
Omar Almatov <omar.almatov@gmail.com>
2022-01-17 23:42:29 +0000

    changed structure

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import CreatePost from './pages/CreatePost';
+import { useState } from 'react';
+
+function App() {
+    const [isAuth, setIsAuth] = useState(false);
+
+    return (
+        <Router>
+            <nav>
+                <Link to={'/'}>Home</Link>
+                <Link to={'/login'}>Login</Link>
+                <Link to={'/createpost'}>CreatePost</Link>
+            </nav>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route
+                    path="/login"
+                    element={<Login setIsAuth={setIsAuth} />}
+                />
+                <Route path="/createpost" element={<CreatePost />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
