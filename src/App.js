c7ef904ac300ad928bb2a6dace092047925699c6
Omar Almatov <omar.almatov@gmail.com>
2022-01-17 20:03:52 -0800

    removed docker

--- src/App.js
+++ src/App.js
@@ -4,16 +4,32 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import CreatePost from './pages/CreatePost';
 import { useState } from 'react';
+import { signOut } from 'firebase/auth';
+import { auth } from './firebase-config';
 
 function App() {
     const [isAuth, setIsAuth] = useState(false);
+
+    const signUserOut = () => {
+        signOut(auth).then(() => {
+            localStorage.clear();
+            setIsAuth(false);
+            window.location.pathname = '/login';
+        });
+    };
 
     return (
         <Router>
             <nav>
                 <Link to={'/'}>Home</Link>
-                <Link to={'/login'}>Login</Link>
-                <Link to={'/createpost'}>CreatePost</Link>
+                {!isAuth ? (
+                    <Link to={'/login'}>Login</Link>
+                ) : (
+                    <>
+                        <Link to={'/createpost'}>CreatePost</Link>
+                        <button onClick={signUserOut}>Log Out</button>
+                    </>
+                )}
             </nav>
             <Routes>
                 <Route path="/" element={<Home />} />
--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,7 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { addDoc, collection } from 'firebase/firestore';
+import { db, auth } from '../firebase-config';
+import { useNavigate } from 'react-router-dom';
 
 function CreatePost() {
-    return <div>Create Post</div>;
+    const [title, setTitle] = useState('');
+    const [postText, setPostText] = useState('');
+
+    const postsCollectionRef = collection(db, 'posts');
+    let navigate = useNavigate();
+
+    const createPost = async () => {
+        await addDoc(postsCollectionRef, {
+            title,
+            postText,
+            author: {
+                name: auth.currentUser.displayName,
+                id: auth.currentUser.uid,
+            },
+        });
+        navigate('/');
+    };
+
+    return (
+        <div className="createPostPage">
+            <div className="cpContainer">
+                <h1>Create A Post</h1>
+                <div className="inputGp">
+                    <label>Title:</label>
+                    <input
+                        placeholder="Title..."
+                        onChange={(event) => {
+                            setTitle(event.target.value);
+                        }}
+                    />
+                </div>
+                <div className="inputGp">
+                    <label>Post:</label>
+                    <textarea
+                        placeholder="Post..."
+                        onChange={(event) => {
+                            setPostText(event.target.value);
+                        }}
+                    />
+                </div>
+                <button onClick={createPost}>Submit Post</button>
+            </div>
+        </div>
+    );
 }
 
 export default CreatePost;
