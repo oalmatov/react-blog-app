72188357077f38d2dd4e1244acd5ca63f3c573ef
Omar Almatov <omar.almatov@berkeley.edu>
2022-01-23 20:40:06 -0800

    cleaned up firebase-config and edited README

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-//import { getAnalytics } from "firebase/analytics";
 
 import { getFirestore } from 'firebase/firestore';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
@@ -21,7 +20,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-//const analytics = getAnalytics(app);
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
